f0acb906ec09d3a949da88ba8a35efe38f7d4b04
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 01:28:23 +0300

    Update bilet46.cpp

--- bilets/bilet46.cpp
+++ bilets/bilet46.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+struct cListNode{
+	int index;
+	int num;
+	cListNode *left;
+	cListNode *right;
+};
+
+int findZerosFromTo(int i, int j, cListNode* head){
+	int index = 1;
+	int counter = 0;
+	
+	while (head and head->index <= j)
+	{
+		if (head->index >= i)
+			counter++;
+		head = head->left;
+	}
+	
+	return j-i-counter+1 ;
+}
