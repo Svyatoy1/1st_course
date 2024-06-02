742cef9483fb90c757fdcc2261671f502d0228d0
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 16:39:48 +0300

    Update bilet24_NOT_FINISHED.cpp

--- bilets/bilet24_NOT_FINISHED.cpp
+++ bilets/bilet24_NOT_FINISHED.cpp
@@ -4,3 +4,13 @@ using namespace std;
 //task1 
 /* До лінійного списку F з m цілих чисел, більшість елементів якого дорівнює 0,
    застосоване стисле зберігання. Написати функцію для визначення i-го елемента списку */
+
+int getElementAtIndex(cListNode* cHead, int index) {
+    cListNode* temp = cHead;
+    while (temp) {
+        if (temp->index == index) {
+            return temp->num;
+        }
+        temp = temp->left;
+    }
+    return 0; // якщо елемент з таким індексом не знайдено, повертаємо 0
