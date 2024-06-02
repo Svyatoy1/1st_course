f1804e9eb944d9abe304582dd158abbe62f7bad4
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 18:04:25 +0300

    Take from Pasha's bilet task1 bilet2_2020.cpp

--- bilets/bilet2_2020.cpp
+++ bilets/bilet2_2020.cpp
@@ -1,3 +1,43 @@
 #include <iostream>
 using namespace std;
 
+//task1 
+/* В елементах зв'язного списку розміщені різні цілі числа. Написати функцію, що роздруковує
+   значення елементів, розміщених між найменшим та найбільшим елементами списку */
+
+void printElementsBetweenMinAndMax(Node* head){
+    Node* current = head;
+    int minIndex = 0;
+    int maxIndex = 0;
+    int currentIndex = 0;
+    Node *minNode = head;
+    Node *maxNode = head;
+
+    while (current){
+        if(current->data < minNode->data){
+            minNode = current;
+            minIndex = currentIndex;
+        }else if(current->data > maxNode->data){
+            maxNode = current;
+            maxIndex = currentIndex;
+        }
+        currentIndex++;
+        current = current->next;
+    }
+
+    Node* startFrom;
+    Node* endBy;
+    if(maxIndex > minIndex){
+        startFrom = minNode;
+        endBy = maxNode;
+    }else{
+        startFrom = maxNode;
+        endBy = minNode;
+    }
+    Node* currentToPrint = startFrom;
+
+    while (currentToPrint!=endBy->next){
+        cout<< currentToPrint->data << "->";
+        currentToPrint = currentToPrint->next;
+    }
+}
