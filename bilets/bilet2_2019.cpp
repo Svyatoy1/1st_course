8be5fe57e7a040d787f412de7cf9edae1d723225
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 20:05:58 +0300

    Update bilet2_2019.cpp

--- bilets/bilet2_2019.cpp
+++ bilets/bilet2_2019.cpp
@@ -3,3 +3,30 @@ using namespace std;
 
 //task1
 /* Написати функцію для впорядкування елементів однозв'язного списку за зростанням */
+
+nodeStack* insertionSort(nodeStack* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    nodeStack* sorted = NULL;
+    nodeStack* current = head;
+
+    while (current) {
+        nodeStack* next = current->next;
+
+        if (sorted == NULL || current->num < sorted->num) {
+            current->next = sorted;
+            sorted = current;
+        } else {
+            nodeStack* temp = sorted;
+            while (temp->next != NULL && temp->next->num < current->num) {
+                temp = temp->next;
+            }
+            current->next = temp->next;
+            temp->next = current;
+        }
+        current = next;
+    }
+    return sorted;
+}
