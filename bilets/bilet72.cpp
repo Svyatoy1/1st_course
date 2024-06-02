5343b6bc5e3ed14c80a57ee6c5629047a8bd9266
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 15:07:32 +0300

    Update bilet72.cpp

--- bilets/bilet72.cpp
+++ bilets/bilet72.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 using namespace std;
 
+
+
+grph matrixToStruct(int mtr[8][8]){
+    grph graph;
+    for (int i=0; i<8; i++)
+        graph.arr[i] = NULL;
+
+    for (int i=0; i<8; i++){
+        node* tempLast = NULL;
+        for (int j=0; j<8; j++){
+            if (mtr[i][j] != 0){
+                node* tempNew = new node;
+                tempNew->num = j;
+                tempNew->next = NULL;
+
+                if(!graph.arr[i])
+                    graph.arr[i] = tempNew;
+                else
+                    tempLast->next = tempNew;
+                tempLast = tempNew;
+            }
+        }
+    }
+    return graph;
+}
