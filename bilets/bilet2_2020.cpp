ab1b49941f8ad742432fc2b03be0cde2dbe0e1c8
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 18:19:45 +0300

    Update bilet2_2020.cpp

--- bilets/bilet2_2020.cpp
+++ bilets/bilet2_2020.cpp
@@ -47,3 +47,47 @@ void printElementsBetweenMinAndMax(Node* head){
 відмітки вузлів бінарного дерева, поданого у "стандартній формі", 
 по рівнях (починаючи з кореня дерева, далі з синів кореня й далі) */
 
+struct st
+{
+	stecT* next;
+	node* node;
+};
+
+void stecTADD(stecT** teil,node* node)
+{
+	stecT* temp = new stecT;
+	temp->next = NULL;
+	temp->node = node;
+	(*teil)->next = temp;
+	(*teil) = temp;
+}
+
+void stecTDell(stecT** head,stecT** teil)
+{
+	stecT* temp = (*head);
+	if ((*head) == (*teil))
+	{
+		(*teil) = NULL;
+	}
+	(*head) = (*head)->next;
+	delete temp;
+}
+
+void StecTShow(node* root)
+{
+	stecT* head = new stecT;
+	head->next = NULL;
+	head->node = root;
+	stecT* teil = head;
+
+	while (head)
+	{
+		if (head->node->left)
+			stecTADD(&teil, head->node->left);
+		if (head->node->right)
+			stecTADD(&teil, head->node->right);
+		cout << head->node->num << ", ";
+		stecTDell(&head, &teil);
+	}
+
+}
