a9781cd92df8bff1aed801ad0984f1f9580043e9
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-02 12:04:25 +0300

    Task1 solved

--- bilets/bilet46.cpp
+++ bilets/bilet46.cpp
@@ -5,6 +5,12 @@ using namespace std;
    застосоване "стисле зв'язне зберігання". Написати функцію для визначення кількості
    елементів зі значенням 0, номери яких належать інтервалу [i,j]. */
 
+struct listNode {
+    int num;
+    listNode* left;
+    listNode* right;
+};
+
 struct cListNode{
 	int index;
 	int num;
@@ -12,26 +18,54 @@ struct cListNode{
 	cListNode *right;
 };
 
-int findZerosFromTo(int i, int j, cListNode* head){
-	int counter = 1;
-	int numberOfZeros = 0;
+void cListAdd(int value, int index, cListNode** head, cListNode** tail) {
+    cListNode* temp = new cListNode;
+    temp->num = value;
+    temp->index = index;
 
-	while (head){
-		if (head->right == NULL && ((head->index) - counter + 1) > i){
-			numberOfZeros++;
-			counter++;
-		}
-		if (head->right == NULL && ((head->index) - counter + 1) == i)
-			counter = 1;
-		if (head->index >= i && head->index <=j){
-			if (head->right && ((head->index) - counter) != (head->right->index)){
-				numberOfZeros++;
-				counter++;
-			}
-			counter = 1;
-		}
-		head = head->left;
-	}
-	
-	return numberOfZeros;
+    if (!(*head)) {
+        *head = temp;
+        *tail = temp;
+        temp->left = NULL;
+        temp->right = NULL;
+    } else {
+        temp->right = *tail;
+        temp->left = NULL;
+        (*tail)->left = temp;
+        *tail = temp;
+    }
+}
+
+void cListNew(cListNode** cHead, cListNode** cTail, listNode* head) {
+    listNode* temp = head;
+    int index = 1;
+
+    while (temp) {
+        if (temp->num != 0) {
+            cListAdd(temp->num, index, cHead, cTail);
+        }
+        index++;
+        temp = temp->left;
+    }
+}
+
+int findZerosFromTo(int i, int j, cListNode* head) {
+    int counter = i;
+    int numberOfZeros = 0;
+    while (head && head->index <= j) {
+        if (head->index >= i) {
+            while (counter < head->index) {
+                numberOfZeros++;
+                counter++;
+            }
+            counter++; // move to the next expected index
+        }
+        head = head->left;
+    }
+    // Account for any remaining zeros in the range
+    while (counter <= j) {
+        numberOfZeros++;
+        counter++;
+    }
+    return numberOfZeros;
 }
