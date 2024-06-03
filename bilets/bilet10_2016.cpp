57e41d7f8e99d43bb7b930364f914a87c944af68
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-03 16:51:05 +0300

    Update bilet10_2016.cpp

--- bilets/bilet10_2016.cpp
+++ bilets/bilet10_2016.cpp
@@ -1,2 +1,29 @@
 #include <iostream>
 using namespace std;
+
+void AddElementInList(list** head, list** teil, int number) {
+	list* temp = new list;
+	temp->num = number;
+	if (*head) {
+		(*head)->right = temp;
+		(*teil)->left = temp;
+		temp->left = (*head);
+		temp->right = (*teil);
+		(*teil) = temp;
+	} else {
+		(*teil) = temp;
+		(*head) = (*teil);
+		(*head)->left = (*teil);
+		(*head)->right = (*teil);
+		(*teil)->left = (*head);
+		(*teil)->right = (*head);
+	}
+}
+
+void CopyList(list* head, list* teil, list** headc, list** teilc) {
+	do {
+		AddOB(headc, teilc, teil->num);
+		teil = teil->right;
+	} while 
+      (teil != head->right);
+}
