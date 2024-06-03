03bb62b2b8fbcbc7ef7e00302663fea6ea0af6fa
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-03 17:08:31 +0300

    Update bilet10_2016.cpp

--- bilets/bilet10_2016.cpp
+++ bilets/bilet10_2016.cpp
@@ -10,6 +10,7 @@ using namespace std;
    застосоване "стисле зв'язне зберігання". Написати функцію для визначення кількості
    елементів зі значенням 0, номери яких належать інтервалу [i,j]. */
 
+//task 1
 void AddElementInList(list** head, list** tail, int number) {
 	list* temp = new list;
 	temp->num = number;
@@ -35,4 +36,26 @@ void CopyList(list* head, list* tail, list** headc, list** tailc) {
 		tail = tail->right;
 	} while 
       (tail != head->right);
+}
+
+//task 2
+int findZerosFromTo(int i, int j, cListNode* head) {
+    int counter = i;
+    int numberOfZeros = 0;
+    while (head && head->index <= j) {
+        if (head->index >= i) {
+            while (counter < head->index) {
+                numberOfZeros++;
+                counter++;
+            }
+            counter++; // move to the next expected index
+        }
+        head = head->left;
+    }
+    // Account for any remaining zeros in the range
+    while (counter <= j) {
+        numberOfZeros++;
+        counter++;
+    }
+    return numberOfZeros;
 }
