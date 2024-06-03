3a9e9e6fa15aac9e3bf42d3f1183272fac97d377
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-03 12:02:43 +0300

    Update bilet46.cpp

--- bilets/bilet46.cpp
+++ bilets/bilet46.cpp
@@ -6,6 +6,15 @@ using namespace std;
    застосоване "стисле зв'язне зберігання". Написати функцію для визначення кількості
    елементів зі значенням 0, номери яких належать інтервалу [i,j]. */
 
+//task2
+/* Написати функцію для обчислення значення виразу з цілих чисел та операцій "+", "*",
+записаного у формі ПОЛІЗ. Потрібний стек реалізуват власноруч */
+
+//task3
+/* Написати функцію, яка для орієнтованого графа будує орієнтований граф
+   з протилежною орієнтацією дуг. Графи представлені "структурами суміжності" */
+
+//task 1
 struct listNode {
     int num;
     listNode* left;
@@ -73,9 +82,6 @@ int findZerosFromTo(int i, int j, cListNode* head) {
 }
 
 //task2
-/* Написати функцію для обчислення значення виразу з цілих чисел та операцій "+", "*",
-записаного у формі ПОЛІЗ. Потрібний стек реалізуват власноруч */
-
 struct number{
 	int num;
 	number* next;
@@ -123,9 +129,6 @@ int countPOLIZ(char in[100]){
 }
 
 //task3
-/* Написати функцію, яка для орієнтованого графа будує орієнтований граф
-   з протилежною орієнтацією дуг. Графи представлені "структурами суміжності" */
-
 struct node {
     int num; // index of matrix column
     node* next; // pointer to next element in the same row
@@ -134,50 +137,6 @@ struct node {
 struct grph {
     node* arr[8];
 };
-
-void showMatrix (int mtr[8][8]){
-    for (int i=0; i<8; i++){
-        for (int j=0; j<8; j++)
-            cout << mtr[i][j];
-        cout << endl;
-    }
-}
-
-grph matrixToStruct(int mtr[8][8]){
-    grph graph;
-    for (int i=0; i<8; i++)
-        graph.arr[i] = NULL;
-
-    for (int i=0; i<8; i++){
-        node* tempLast = NULL;
-        for (int j=0; j<8; j++){
-            if (mtr[i][j] != 0){
-                node* tempNew = new node;
-                tempNew->num = j;
-                tempNew->next = NULL;
-
-                if(!graph.arr[i])
-                    graph.arr[i] = tempNew;
-                else
-                    tempLast->next = tempNew;
-                tempLast = tempNew;
-            }
-        }
-    }
-    return graph;
-}
-
-void showStruct (grph Graph){
-    for (int i=0; i<8; i++){
-        cout << "point: " << i+1 << " -> ";
-        node* temp = Graph.arr[i];
-        while (temp){
-            cout << temp->num+1 << "; ";
-            temp = temp->next;
-        }
-        cout << endl;
-    }
-}
 
 // MAIN FUNCTION
 grph arcReverseOrientation(grph graph) {
@@ -208,7 +167,6 @@ grph arcReverseOrientation(grph graph) {
     }
     return reverse;
 }
-
 
 int main () {
 //task1
