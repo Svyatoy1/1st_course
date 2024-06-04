aec5158f3986bfd3fd45ac327ba20e155ab1ad66
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-04 15:56:44 +0300

    Update bilet7_2016.cpp

--- bilets/bilet7_2016.cpp
+++ bilets/bilet7_2016.cpp
@@ -6,3 +6,18 @@ using namespace std;
 поданих зв'язними впорядкованими списками, знаходить їхній перетин*/
 
 //task1
+void multilicationOfLists(list* head1, list* head2) {
+	while (head1 && head2) {
+		if (head1->num == head2->num) {
+			cout << head1->num<<", ";
+			head1 = head1->left;
+			head2 = head2->left;
+		} else {
+      if (head1->num > head2->num)
+				head2 = head2->left;
+			else
+				head1 = head1->left;
+    }
+	}
+	cout << endl;
+}
