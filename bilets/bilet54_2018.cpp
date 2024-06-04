3c9e30cc8a081f338bdf265f6e6d4c9425d22f0c
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-04 23:11:57 +0300

    Update bilet54_2018.cpp

--- bilets/bilet54_2018.cpp
+++ bilets/bilet54_2018.cpp
@@ -6,6 +6,11 @@ using namespace std;
 впорядкованим за зростанням степені змінної, без зберігання одночленів із з нульовими 
 коефіцієнтами. Написати функцію, яка виводить степінь многочлена */
 
+//task 2
+/* До лінійного списку F з m цілих чисел, більшість елементів якого дорівнюють 0,
+   застосоване стисле зв'язне зберігання. Написати функцію для визначення номера
+   першого за порядком елемента списку F із значенням 0 */
+
 //task1
 struct Node {
     int coefficient; 
@@ -14,9 +19,9 @@ struct Node {
 };
 
 int getPolynomialDegree(Node* head) {
-    if (head == NULL) {
+    if (head == NULL) 
         return -1;
-    }
+    
     Node* temp = head;
     while (temp->next) {
         temp = temp->next;
@@ -24,3 +29,21 @@ int getPolynomialDegree(Node* head) {
     return temp->exponent;
 }
 
+//task2
+int findFirstZero(Node* head, int m) {
+    int currentIndex = 1; //numeration starts from 1
+    Node* temp = head;
+    
+    while (temp && currentIndex < m) {
+        if (temp->index == currentIndex) 
+            temp = temp->next;
+         else 
+            return currentIndex;
+        currentIndex++;
+    }
+    
+    if (currentIndex < m) 
+        return currentIndex; //if we reach end of list, we return index of -1 unless we find zeros
+     else 
+        return -1;
+}
