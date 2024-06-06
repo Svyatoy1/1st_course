a71d8dd386f5e48b242ec0875a4b7f6cbb11efca
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-06 15:00:19 +0300

    Update bilet20_2018.cpp

--- bilets/bilet20_2018.cpp
+++ bilets/bilet20_2018.cpp
@@ -5,68 +5,38 @@ using namespace std;
 /* В елементах циклічного списку розміщені цілі числа. Написали функцію, що перевпорядковує список, 
 збираючи спочатку всі від'ємні числа, потім - 0, а потім додатні */
 
-//task1
-void reorderList(Node** head) {
-    if (!head || !*head) {
-        return;
-    }
+void moveList(list** head, list** tail) {
+    list* current = *head;
+    list* lastNegative = NULL;
 
-    Node* negHead = NULL, *negTail = NULL;
-    Node* zeroHead = NULL, *zeroTail = NULL;
-    Node* posHead = NULL, *posTail = NULL;
-    Node* current = *head;
-
-    do {
-        Node* next = current->next;
-        if (current->data < 0) {
-            if (!negHead) {
-                negHead = negTail = current;
+    while (current) {
+        if (current->num < 0) {
+            if (lastNegative == NULL) {
+                lastNegative = current;
             } else {
-                negTail->next = current;
-                negTail = current;
-            }
-        } else if (current->data == 0) {
-            if (!zeroHead) {
-                zeroHead = zeroTail = current;
-            } else {
-                zeroTail->next = current;
-                zeroTail = current;
-            }
-        } else {
-            if (!posHead) {
-                posHead = posTail = current;
-            } else {
-                posTail->next = current;
-                posTail = current;
+                if (lastNegative->right != current) {
+                    if (current->left) 
+			current->left->right = current->right;
+                    if (current->right) 
+			current->right->left = current->left;
+                  
+                    current->left = lastNegative;
+                    current->right = lastNegative->right;
+                  
+                    if (lastNegative->right) 
+			lastNegative->right->left = current;
+                  
+                    lastNegative->right = current;
+                  
+                    if (lastNegative == *tail) 
+			*tail = current;
+                  
+                    lastNegative = current;
+                } else {
+                    lastNegative = current;
+                }
             }
         }
-        current = next;
-    } while (current != *head);
-
-    // Connect the three lists
-    if (negTail) {
-        negTail->next = zeroHead ? zeroHead : posHead;
+        current = current->right;
     }
-    if (zeroTail) {
-        zeroTail->next = posHead;
-    }
-    if (posTail) {
-        posTail->next = negHead ? negHead : zeroHead;
-    }
-
-    // Set new head
-    if (negHead) {
-        *head = negHead;
-    } else if (zeroHead) {
-        *head = zeroHead;
-    } else {
-        *head = posHead;
-    }
-
-    // Make the list circular again
-    Node* temp = *head;
-    while (temp->next && temp->next != *head) {
-        temp = temp->next;
-    }
-    temp->next = *head;
 }
