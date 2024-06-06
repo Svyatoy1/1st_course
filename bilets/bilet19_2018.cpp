e48b81a31c1660dce1f3e70e108060d82390f876
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-06 14:37:35 +0300

    Update bilet19_2018.cpp

--- bilets/bilet19_2018.cpp
+++ bilets/bilet19_2018.cpp
@@ -152,3 +152,27 @@ int countPositiveValues(Node* root) {
 }
 
 //task5
+void DFS(grph graph, int active, bool check[8]) {
+    check[active - 1] = true;
+
+    node* temp = graph.arr[active - 1];
+    while (temp) {
+        if (!check[temp->num - 1])
+            DFS(graph, temp->num, check);
+        temp = temp->next;
+    }
+}
+
+bool isGraphConnected(grph& graph) {
+    bool check[8] = {false};
+   
+    int startNode = 1;
+    DFS(graph, startNode, check);
+   
+    for (int i = 0; i < 8; i++) {
+        if (!check[i]) {
+            return false;
+        }
+    }
+    return true;
+}
