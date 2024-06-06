c42419221ef4a177670c4fbf93ea064925fb1a59
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-06 14:52:40 +0300

    Update bilet20_2018.cpp

--- bilets/bilet20_2018.cpp
+++ bilets/bilet20_2018.cpp
@@ -6,3 +6,46 @@ using namespace std;
 збираючи спочатку всі від'ємні числа, потім - 0, а потім додатні */
 
 //task1
+void reorderList(Node** head) {
+    if (!head || !*head) {
+        return;
+    }
+
+    Node* negHead = NULL, *negTail = NULL;
+    Node* zeroHead = NULL, *zeroTail = NULL;
+    Node* posHead = NULL, *posTail = NULL;
+    Node* current = *head;
+
+    do {
+        Node* next = current->next;
+        if (current->data < 0) {
+            if (!negHead) {
+                negHead = negTail = current;
+            } else {
+                negTail->next = current;
+                negTail = current;
+            }
+        } else if (current->data == 0) {
+            if (!zeroHead) {
+                zeroHead = zeroTail = current;
+            } else {
+                zeroTail->next = current;
+                zeroTail = current;
+            }
+        } else {
+            if (!posHead) {
+                posHead = posTail = current;
+            } else {
+                posTail->next = current;
+                posTail = current;
+            }
+        }
+        current = next;
+    } while (current != *head);
+
+    if (negTail) negTail->next = zeroHead ? zeroHead : posHead;
+    if (zeroTail) zeroTail->next = posHead;
+    if (posTail) posTail->next = negHead ? negHead : (zeroHead ? zeroHead : posHead);
+
+    *head = negHead ? negHead : (zeroHead ? zeroHead : posHead);
+}
