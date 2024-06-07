f0a4bcba05d9ebcc63abf6475e11e634d3578848
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 00:53:17 +0300

    Update bilet9_2019.cpp

--- bilets/bilet9_2019.cpp
+++ bilets/bilet9_2019.cpp
@@ -5,6 +5,10 @@ using namespace std;
 /* Здійснити злиття двох впорядкованих списків у зв'язному зберіганні в один, який також впорядкований 
 й представлений новим списком */
 
+//task2
+/* Написати функцію для побудови послідовно-зв'язного індексного зберігання розрідженої матриці 
+В[10,40] за звичайним представленням двовимірним масивом */
+
 //task1
 ListNode* mergeSortedLists(ListNode* list1, ListNode* list2) {
     if (!list1) return l2;
@@ -39,4 +43,62 @@ ListNode* mergeSortedLists(ListNode* list1, ListNode* list2) {
         mergedTail->next = list2;
 
     return mergedHead;
+}
+
+//task2
+struct ListNode {
+    int value;
+    int rowIndex;
+    int colIndex;
+    ListNode* next;
+    ListNode* prev;
+};
+
+struct MatNode {
+    ListNode* rowList;
+    MatNode* next;
+};
+
+void AddElement(ListNode** head, ListNode** tail, int value, int rowIndex, int colIndex) {
+    ListNode* newNode = new ListNode;
+    newNode->value = value;
+    newNode->rowIndex = rowIndex;
+    newNode->colIndex = colIndex;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    if (*head) {
+        (*tail)->next = newNode;
+        newNode->prev = *tail;
+        *tail = newNode;
+    } else {
+        *head = newNode;
+        *tail = newNode;
+    }
+}
+
+ListNode* CreateRowList(ListNode** head, ListNode** tail, const int rowIndex, const int arr[], const int length) {
+    for (int i = 0; i < length; i++) {
+        if (arr[i] != 0)
+            AddElement(head, tail, arr[i], rowIndex, i);
+    }
+    return *head;
+}
+
+void CreateSparseMatrix(MatNode** headMat, MatNode** tailMat, int numRows, int numCols, int matrix[10][40]) {
+    for (int i = 0; i < numRows; i++) {
+        ListNode* rowHead = NULL;
+        ListNode* rowTail = NULL;
+        MatNode* newMatNode = new MatNode;
+        newMatNode->rowList = CreateRowList(&rowHead, &rowTail, i, matrix[i], numCols);
+        newMatNode->next = NULL;
+
+        if (*headMat) {
+            (*tailMat)->next = newMatNode;
+            *tailMat = newMatNode;
+        } else {
+            *headMat = newMatNode;
+            *tailMat = newMatNode;
+        }
+    }
 }
