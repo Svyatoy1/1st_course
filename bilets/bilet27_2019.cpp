beade40f36005028d7adeb23b01062a8ff780c1f
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 01:15:02 +0300

    Update bilet27_2019.cpp

--- bilets/bilet27_2019.cpp
+++ bilets/bilet27_2019.cpp
@@ -6,6 +6,10 @@ using namespace std;
 впорядкованим за зростанням степені змінної, без зберігання одночленів із з нульовими 
 коефіцієнтами. Написати функцію, яка виводить степінь полінома */
 
+//task2
+/* Написати функцію для реалізації поелементного додавання двох розріджених матриць,
+при послідовно-зв'язному індексному зберіганні */
+
 //task1
 struct Node {
     int coefficient; 
@@ -22,4 +26,74 @@ int getPolynomialDegree(Node* head) {
         temp = temp->next;
     }
     return temp->exponent;
+}
+
+//task2
+struct Node {
+    int value;
+    int row;
+    int col;
+    Node* prev;
+    Node* next;
+};
+
+struct MatrixRow {
+    Node* nodes;
+    MatrixRow* next;
+};
+
+void addNode(Node** head, Node** tail, int value, int row, int col) {
+    Node* newNode = new Node;
+    newNode->value = value;
+    newNode->row = row;
+    newNode->col = col;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    if (*head) {
+        (*tail)->next = newNode;
+        newNode->prev = *tail;
+        *tail = newNode;
+    } else {
+        *head = newNode;
+        *tail = *head;
+    }
+}
+
+void addMatrices(MatrixRow* matA, MatrixRow* matB, MatrixRow** resultHead, MatrixRow** resultTail) {
+    while (matA || matB) {
+        Node* nodeA = NULL;
+        Node* nodeB = NULL;
+        if (matA) nodeA = matA->nodes;
+        if (matB) nodeB = matB->nodes;
+        Node* resHead = NULL;
+        Node* resTail = NULL;
+        MatrixRow* newRow = new MatrixRow;
+
+        while (nodeA || nodeB) {
+            if (nodeA && nodeB && nodeA->row == nodeB->row && nodeA->col == nodeB->col) {
+                addNode(&resHead, &resTail, nodeA->value + nodeB->value, nodeA->row, nodeA->col);
+                nodeA = nodeA->next;
+                nodeB = nodeB->next;
+            } else if (nodeB && (!nodeA || (nodeA->row > nodeB->row || (nodeA->row == nodeB->row && nodeA->col > nodeB->col)))) {
+                addNode(&resHead, &resTail, nodeB->value, nodeB->row, nodeB->col);
+                nodeB = nodeB->next;
+            } else {
+                addNode(&resHead, &resTail, nodeA->value, nodeA->row, nodeA->col);
+                nodeA = nodeA->next;
+            }
+        }
+
+        newRow->nodes = resHead;
+        newRow->next = NULL;
+        if (*resultHead) {
+            (*resultTail)->next = newRow;
+            *resultTail = newRow;
+        } else {
+            *resultHead = newRow;
+            *resultTail = *resultHead;
+        }
+
+        if (matA) matA = matA->next;
+        if (matB) matB = matB->next;
+    }
 }
