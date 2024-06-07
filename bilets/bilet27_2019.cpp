d4d7bad493a2e7151dce2ec15fbaada8f49edaa7
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 01:06:03 +0300

    Update bilet27_2019.cpp

--- bilets/bilet27_2019.cpp
+++ bilets/bilet27_2019.cpp
@@ -5,3 +5,21 @@ using namespace std;
 /* Поліном від однієї змінної з цілими коефіцієнтами можна подати зв'язним списком, 
 впорядкованим за зростанням степені змінної, без зберігання одночленів із з нульовими 
 коефіцієнтами. Написати функцію, яка виводить степінь полінома */
+
+//task1
+struct Node {
+    int coefficient; 
+    int exponent;    
+    Node* next;      
+};
+
+int getPolynomialDegree(Node* head) {
+    if (head == NULL) 
+        return -1;
+    
+    Node* temp = head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    return temp->exponent;
+}
