1568a5f373cd970d391b696dfec3292261a9ab83
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 22:45:54 +0300

    Update bilet22.cpp

--- bilets/bilet22.cpp
+++ bilets/bilet22.cpp
@@ -4,6 +4,9 @@ using namespace std;
 /* Написати функцію для копіювання списку у «однозвʼязному представленні», зберігаючи 
 взаємний порядок елементів, але зробивши у копії «двозвʼязне циклічне представлення» */
 
+/* Написати функцію для визначення висоти «дерева степені 3», що зберігається у 
+«розширеній стандартній формі». */
+
 //task1
 struct stack {
     int num;
@@ -39,4 +42,24 @@ void copyToDoubleCircular(stack* stackHead, list** head, list** tail) {
         *tail = newNode;
         stackHead = stackHead->next;
     }
+}
+
+//task2
+struct TreeNode {
+    int value;
+    TreeNode* parent;
+    TreeNode* child1;
+    TreeNode* child2;
+    TreeNode* child3;
+};
+
+int heightOfTree(TreeNode* node) {
+    if (node == NULL)
+        return 0;
+
+    int height1 = heightOfTree(node->child1);
+    int height2 = heightOfTree(node->child2);
+    int height3 = heightOfTree(node->child3);
+
+    return 1 + max({height1, height2, height3});
 }
