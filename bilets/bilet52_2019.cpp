1852d7afb87153d0fb1992e6796f7d3a0cb5927c
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 13:19:31 +0300

    Update bilet52_2019.cpp

--- bilets/bilet52_2019.cpp
+++ bilets/bilet52_2019.cpp
@@ -38,3 +38,82 @@ void sortCycleList(list** head, list** tail) {
   
 	(*head) = start;
 }
+
+//task2
+// Додавання вузла в стек
+void pushStack(StackNode** head, StackNode** tail, TreeNode* treeNode) {
+    StackNode* newNode = new StackNode;
+    newNode->treeNode = treeNode;
+    newNode->next = NULL;
+    if (*head == NULL) {
+        *head = newNode;
+        *tail = newNode;
+    } else {
+        newNode->next = *tail;
+        *tail = newNode;
+    }
+}
+
+// Видалення вузла зі стека
+void popStack(StackNode** head, StackNode** tail) {
+    if (*tail == NULL) 
+	    return;
+    StackNode* temp = *tail;
+    *tail = (*tail)->next;
+    if (*tail == NULL) 
+	    *head = NULL;
+    delete temp;
+}
+
+// Функція для зворотного проходження дерева
+void traverseReverse(TreeNode* root, StackNode** head, StackNode** tail) {
+    if (root == NULL) 
+	    return;
+    StackNode* stack = NULL;
+    pushStack(&stack, &stack, root);
+
+    while (stack) {
+        TreeNode* currentNode = stack->treeNode;
+        popStack(&stack, &stack);
+        pushStack(head, tail, currentNode);
+        if (currentNode->left != NULL) 
+            pushStack(&stack, &stack, currentNode->left);
+        if (currentNode->right != NULL) 
+            pushStack(&stack, &stack, currentNode->right);
+    }
+}
+
+// Функція для друкування відміток вузлів у зворотному порядку
+void printReverse(TreeNode* root) {
+    StackNode* head = NULL;
+    StackNode* tail = NULL;
+    traverseReverse(root, &head, &tail);
+    while (tail) {
+        cout << tail->treeNode->value << ", ";
+        popStack(&head, &tail);
+    }
+    cout << endl;
+}
+
+//task3
+void DFS(grph graph, int vertex, bool visited[10]) { //assuming graph contains 10 nodes
+    visited[vertex - 1] = true;
+    Node* temp = graph.arr[vertex - 1];
+    while (temp) {
+        if (!visited[temp->num - 1])
+            DFS(graph, temp->num, visited);
+        temp = temp->next;
+    }
+}
+
+// Функція для виведення досяжних вершин з даної вершини
+void reachableVertices(grph graph, int startVertex, bool visited[10]) {
+    DFS(graph, startVertex, visited);
+    cout << "The vertices reachable from vertex " << startVertex << " are: ";
+    for (int i = 0; i < 10; i++) {
+        if (visited[i]) {
+            cout << i + 1 << ", ";
+        }
+    }
+    cout << endl;
+}
