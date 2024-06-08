90c4853f10a52e3555d356ef5e9d356e3d20f532
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 19:16:02 +0300

    Update bilet6.cpp

--- bilets/bilet6.cpp
+++ bilets/bilet6.cpp
@@ -5,6 +5,10 @@ using namespace std;
 /* Написати функцію, яка у списку, що зберігається у зв'язному представленні, з кожної групи 
 сусідніх однакових елементів залишає лише один. */
 
+//task2
+/* Написати функцію, для обчислення матриці В=А2, де для матриці А використано "прямокутне-зв'язне стисле зберігання", 
+а матриця В зберігається традиційним чином у вигляді двовимірного масиву */
+
 //task1
 void leaveOne(list** head, list** tail) {
     list* current = *head;
@@ -22,5 +26,34 @@ void leaveOne(list** head, list** tail) {
             delete temp;
         }
         current = nextNode;
+    }
+}
+
+//task2
+void MatrixQuadro(node* arrR[], node* arrST[], int matrix[10][10]) {
+    // Ініціалізація матриці результатів
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    // Обчислення матриці B = A^2
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            node* tempR = arrR[i];
+            node* tempST = arrST[j];
+            while (tempR && tempST) {
+                if (tempR->j == tempST->i) {
+                    matrix[i][j] += tempR->data * tempST->data; // Накопичення суми добутків
+                    tempR = tempR->nextR;
+                    tempST = tempST->nextST;
+                } else if (tempR->j < tempST->i) {
+                    tempR = tempR->nextR;
+                } else {
+                    tempST = tempST->nextST;
+                }
+            }
+        }
     }
 }
