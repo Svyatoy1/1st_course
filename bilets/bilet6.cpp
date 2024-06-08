4cd9d2c09bf692ac206d06271ff6553fae46721e
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 19:09:48 +0300

    Update bilet6.cpp

--- bilets/bilet6.cpp
+++ bilets/bilet6.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+//task1
+/* Написати функцію, яка у списку, що зберігається у зв'язному представленні, з кожної групи 
+сусідніх однакових елементів залишає лише один. */
+
+//task1
+void leaveOne(list** head, list** tail) {
+    list* current = *head;
+    while (current) {
+        list* nextNode = current->right;
+        while (nextNode && current->num == nextNode->num) {
+            list* temp = nextNode;
+            nextNode = nextNode->right;
+            if (temp->right) {
+                temp->right->left = temp->left;
+            } else {
+                *tail = temp->left; // Update the tail if we are removing the last element
+            }
+            temp->left->right = temp->right;
+            delete temp;
+        }
+        current = nextNode;
+    }
+}
