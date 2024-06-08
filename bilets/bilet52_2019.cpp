349420853b692d973fe42e27f5db08f326b791e9
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 12:57:16 +0300

    Update bilet52_2019.cpp

--- bilets/bilet52_2019.cpp
+++ bilets/bilet52_2019.cpp
@@ -1,3 +1,41 @@
 #include <iostream>
 using namespace std;
 
+//task1
+/* В елементах зв'язного циклічного списку розміщені цілі числа. Напсиати функцію,
+що впорядковує список, збираючи спочатку всі непарні числа, а потім - парні*/
+
+//task2
+/* Написати функцію для знаходження номера рядка матриці з максимальною кількістю 0 
+елементів у розрідженої матриці В[20,50] при стислому зв'язному зберіганні */
+
+//task3
+/* Множина цілих чисел представлена деревом двійкового пошуку. Написати нерекурсивну функцію, 
+що записує у масив цю множину чисел впорядковану за зростанням */
+
+//task1
+void sortCycleList(list** head, list** tail) {
+	list* start = (*head);
+	list* end = (*tail);
+	do {
+		if ((*head)->num % 2 == 0) {
+			list* temp = (*head);
+			if ((*head)->right == (*tail)) {
+				start = (*head)->left;
+				(*tail) = temp;
+			}
+			else {
+				(*head)->left->right = (*head)->right;
+				(*head)->right->left = (*head)->left;
+				(*head) = temp->left;
+				(*tail)->left = temp;
+				temp->right = (*tail);
+				temp->left = start;
+				(*tail) = temp;
+			}
+		}
+		(*head) = (*head)->left;
+	} while ((*head) != end);
+  
+	(*head) = start;
+}
