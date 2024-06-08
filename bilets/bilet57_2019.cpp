710e00d1d07512a0e001d156aa83b78185ef30f4
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 14:28:04 +0300

    Update bilet57_2019.cpp

--- bilets/bilet57_2019.cpp
+++ bilets/bilet57_2019.cpp
@@ -43,3 +43,53 @@ void deleteDivisibleByFive(listI** head, listI** tail) {
 	}
 	(*head) = start;
 }
+
+//task2
+struct poliz {
+	char element;
+	poliz* next;
+};
+
+void polizAdd(poliz** head, char el) {
+	poliz* adder = new poliz;
+	adder->element = el;
+	adder->next = (*head);
+	(*head) = adder;
+}
+
+char getElement(poliz** head) {
+	char element = (*head)->element;
+	poliz* temp = (*head);
+	(*head) = (*head)->next;
+	delete temp;
+	return element;
+}
+
+void initPoliz(char in[100], char out[100]) {
+	poliz* head = NULL;
+	int i = 0;
+	int j = 0;
+
+	while (in[i]) {
+		while (in[i] && (in[i] != '-' && in[i] != '/')) {
+			out[j] = in[i];
+			i++; 
+			j++;
+		}
+		out[j] = ' ';
+		j++;
+		if (in[i]) {
+			while (head && (head->element == '/' || (head->element == in[i]))) {
+				out[j] = getElement(&head);
+				j++;
+			}
+			polizAdd(&head, in[i]);
+			i++;
+		}
+	}
+
+	while (head){
+		out[j] = getElement(&head);
+		j++;
+	}
+}
