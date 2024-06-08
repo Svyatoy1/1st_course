315375f42379793e201c71202fbbadb138f23544
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-08 23:48:39 +0300

    Update bilet32.cpp

--- bilets/bilet32.cpp
+++ bilets/bilet32.cpp
@@ -13,3 +13,47 @@ using namespace std;
 // task 3
 /* Написати функцію, яка вилучає всі петлі «псевдо графа», поданого «структурою 
 суміжності». */
+
+//task1
+struct list {
+	int num;
+	int step;
+	list* left;
+	list* right;
+};
+
+void AddSum(list** head, list** tail, int number, int cont) {
+	list* temp = new list;
+	temp->num = number;
+	temp->left = NULL;
+	
+	if (*head) {
+		temp->power = cont;
+		(*tail)->left = temp;
+		temp->right = (*tail);
+		(*tail) = temp;
+	} else {
+		temp->power = cont;
+		(*head) = temp;
+		(*tail) = (*head);
+		(*head)->right = NULL;
+	}
+}
+
+void sumPol(list** head, list** tail, list* head1, list* tail1, list* head2, list* tail2) {
+	while (head1 || head2) {
+		if (head1->power == head2->power){
+			AddSum(head, tail, head1->num + head2->num,head1->power);
+			head1 = head1->left;
+			head2 = head2->left;
+		}
+		if (head1->power > head2->power) {
+			AddSum(head, tail, head2->num, head2->power);
+			head2 = head2->left;
+		}
+		if (head1->power < head2->power) {
+			AddSum(head, tail, head1->num, head1->power);
+			head1 = head1->left;
+		}
+	}
+}
