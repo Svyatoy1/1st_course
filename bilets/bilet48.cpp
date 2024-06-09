4b16acb7c300bb41adae6c0a3fc02e7ee89396bf
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-09 11:29:41 +0300

    Update bilet48.cpp

--- bilets/bilet48.cpp
+++ bilets/bilet48.cpp
@@ -16,3 +16,57 @@ using namespace std;
 /* Написати функцію, яка для графа визначає всі вершини, відстань яких від заданої
 вершини v становить d. Граф представлений «структурою суміжності». */
 
+//task1
+struct list {
+	int num;
+	list* left;
+	list* right;
+};
+
+void Add(list** head, list** tail, int number) {
+	list* temp = new list;
+	temp->num = number;
+	if (*head) {
+		(*tail)->left = temp;
+		(*head)->right = temp;
+		temp->right = (*tail);
+		temp->left = (*head);
+		(*tail) = temp;
+	}
+	else {
+		temp->left = temp;
+		temp->right = temp;	
+		(*head) = temp;
+		(*tail) = (*head);
+	}
+}
+
+void connection (list** head1, list** tail1, list** head2, list** tail2) {
+	if ((*head1)->right != (*tail2)) {
+		(*head1)->right->left = (*head1)->left;
+		(*head1)->left->right = (*head1)->right;
+	} else {
+		(*tail1)->left = (*head1)->left;
+		(*head1)->left = (*tail1);
+	}
+	Add(head2, tail2, (*head1)->num);
+}
+
+void breaks(list** head_neparni, list** tail_neparni, list** head_parni, list** tail_parni) {
+	if ((*head_neparni)->num % 2 == 0) {
+		do {
+			if ((*head_neparni)->num % 2 != 0) 
+				connection (head_neparni, tail_neparni, head_parni, tail_parni);
+			(*head_neparni) = (*head_neparni)->left;
+
+		} while ((*tail_neparni)->left != (*head_neparni));
+	}
+	else {
+		do {
+			if ((*head_neparni)->num % 2 == 0) 
+				connection (head_neparni, tail_neparni, head_parni, tail_parni);
+			(*head_neparni) = (*head_neparni)->left;
+
+		} while ((*tail_neparni)->left != (*head_neparni));
+	}
+}
