22ea5ab1927ba6f4099cb1ff7eadba8bc2ec90a8
Svyatoy1 <88134893+Svyatoy1@users.noreply.github.com>
2024-06-09 13:00:11 +0300

    Update bilet52.cpp

--- bilets/bilet52.cpp
+++ bilets/bilet52.cpp
@@ -1,2 +1,40 @@
 #include <iostream>
 using namespace std;
+
+// task 1
+/* В елементах звʼязного циклічного списку розміщені цілі числа. Написати функцію, що 
+впорядковує список, збираючи спочатку всі непарні числа, а потім - парні. */
+
+// task 2
+/* Написати не рекурсивну функцію (з використанням стека) для друкування відміток вузлів 
+бінарного дерева, поданого у «стандартній формі», при його проходженні у «оберненому порядку». */
+
+// task 3
+/* Знайти всі вершини графа, що поданий структурую суміжності, які досяжні від заданої вершини. */
+
+//task1
+void sortCycleList(list** head, list** tail) {
+	list* start = (*head);
+	list* end = (*tail);
+	do {
+		if ((*head)->num % 2 == 0) {
+			list* temp = (*head);
+			if ((*head)->right == (*tail)) {
+				start = (*head)->left;
+				(*tail) = temp;
+			}
+			else {
+				(*head)->left->right = (*head)->right;
+				(*head)->right->left = (*head)->left;
+				(*head) = temp->left;
+				(*tail)->left = temp;
+				temp->right = (*tail);
+				temp->left = start;
+				(*tail) = temp;
+			}
+		}
+		(*head) = (*head)->left;
+	} while ((*head) != end);
+  
+	(*head) = start;
+}
